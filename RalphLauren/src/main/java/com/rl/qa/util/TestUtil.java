43fd78de65e618dcdbff017c75cdb5e7c6ebeeaa
madhu <madhu@192.168.1.9>
2018-08-20 19:01:32 +0530

    testData utility_naveen

--- RalphLauren/src/main/java/com/rl/qa/util/TestUtil.java
+++ RalphLauren/src/main/java/com/rl/qa/util/TestUtil.java
@@ -1,16 +1,17 @@
 package com.rl.qa.util;
 
-import java.io.File;
+//import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
+//import java.io.IOException;
+//import java.util.ArrayList;
 
-import org.apache.poi.hssf.usermodel.examples.NewWorkbook;
+//import org.apache.poi.hssf.usermodel.examples.NewWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import org.apache.poi.xssf.usermodel.XSSFSheet;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.rl.qa.base.TestBase;
 
@@ -19,31 +20,62 @@ public class TestUtil extends TestBase
 {
 	public static long PAGE_LOAD_TIMEOUT = 30;
 	public static long IMPLICITLY_WAIT = 20;
-	
-	XSSFSheet sheet;
-	XSSFWorkbook wb;
-	
-	public TestUtil ()
+
+	//	
+	//	XSSFSheet sheet;
+	//	XSSFWorkbook wb;
+	//	
+	//	public TestUtil ()
+	//	{
+	//		try 
+	//		{
+	//			String excelPath = "E:\\rl.xlsx";
+	//			File src = new File (excelPath);
+	//			FileInputStream fis = new FileInputStream(src);
+	//			wb = new XSSFWorkbook(fis);
+	//		} 
+	//		catch (IOException e) 
+	//		{
+	//			System.out.println(e.getMessage());
+	//		}
+	//	}
+	//
+	//	public String getData(int sheetnumber, int row, int column)
+	//	{
+	//		sheet = wb.getSheetAt(sheetnumber);
+	//		String data = sheet.getRow(row).getCell(column).getStringCellValue();
+	//		return data;
+	//	}
+
+	public static String TESTDATA_SHEET_PATH = "E:\\RLGit\\RalphLauren\\src\\main\\java\\com\\rl\\qa\\testdata\\rl.xlsx";
+
+	static Workbook book;
+	static Sheet sheet;
+
+	public static Object[][] getTestData(String sheetName) 
 	{
+		FileInputStream file = null;
 		try 
 		{
-			String excelPath = "E:\\rl.xlsx";
-			File src = new File (excelPath);
-			FileInputStream fis = new FileInputStream(src);
-			wb = new XSSFWorkbook(fis);
-		} 
-		catch (IOException e) 
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) 
 		{
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
-	}
-
-	public String getData(int sheetnumber, int row, int column)
-	{
-		sheet = wb.getSheetAt(sheetnumber);
-		String data = sheet.getRow(row).getCell(column).getStringCellValue();
+		try 
+		{
+			book = WorkbookFactory.create(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		for (int i = 0; i < sheet.getLastRowNum(); i++) 
+		{
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) 
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+		}
 		return data;
 	}
-
 
 }
--- RalphLauren/src/test/java/com/rl/qa/testcases/RegistrationPageTest.java
+++ RalphLauren/src/test/java/com/rl/qa/testcases/RegistrationPageTest.java
@@ -3,6 +3,7 @@ package com.rl.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.rl.qa.base.TestBase;
@@ -15,6 +16,7 @@ public class RegistrationPageTest extends TestBase
 	RegistrationPage registrationpage;
 	LoginPage loginpage;
 	TestUtil util;
+	String sheetName = "CreateUser";
 
 	public RegistrationPageTest()
 	{
@@ -38,31 +40,38 @@ public class RegistrationPageTest extends TestBase
 	//		Assert.assertEquals(atitle, "Ralph Lauren - Account Login | Ralph Lauren | 103.1.11 - controllers");
 	//	}
 
-	@Test
-	public void createUser() throws InterruptedException
+	@DataProvider
+	public Object[][] getTestData()
 	{
-		String fname = util.getData(0, 1, 0);
-		registrationpage.eneterFname(fname);
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+
+	@Test (dataProvider="getTestData")
+	public void createUser(String firstname, String lastname, String emailid, String confirmemailid, String passowrd, String confirmpassword) throws InterruptedException
+	{
+		//String fname = util.getData(0, 1, 0);
+		registrationpage.eneterFname(firstname);
 		Thread.sleep(1000);
 
-		String lname = util.getData(0, 1, 1);
-		registrationpage.eneterlname(lname);
+		//String lname = util.getData(0, 1, 1);
+		registrationpage.eneterlname(lastname);
 		Thread.sleep(1000);
 
-		String email = util.getData(0, 1, 2);
-		registrationpage.eneteremail(email);
+		//String email = util.getData(0, 1, 2);
+		registrationpage.eneteremail(emailid);
 		Thread.sleep(1000);
 
-		String cemail = util.getData(0, 1, 3);
-		registrationpage.confirmemail(cemail);
+		//String cemail = util.getData(0, 1, 3);
+		registrationpage.confirmemail(confirmemailid);
 		Thread.sleep(1000);
 
-		String pwd = util.getData(0, 1, 4);
-		registrationpage.eneterpwd(pwd);
+		//String pwd = util.getData(0, 1, 4);
+		registrationpage.eneterpwd(passowrd);
 		Thread.sleep(1000);
 
-		String cpwd = util.getData(0, 1, 5);
-		registrationpage.confirmpwd(cpwd);
+		//String cpwd = util.getData(0, 1, 5);
+		registrationpage.confirmpwd(confirmpassword);
 		Thread.sleep(1000);
 
 		//registrationpage.clickCheckbox();
@@ -70,7 +79,7 @@ public class RegistrationPageTest extends TestBase
 		registrationpage.clickCreateAccount();
 		String title = driver.getTitle();
 		Assert.assertEquals(title, "Ralph Lauren - Account | Ralph Lauren | 103.1.11 - controllers");
-
+		Thread.sleep(2000);
 	}
 
 	@AfterMethod
