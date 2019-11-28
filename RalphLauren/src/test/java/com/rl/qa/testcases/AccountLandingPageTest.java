3f8175e84579feaf28912bed633ce15fd337f1fe
madhu <madhu7973@gmail.com>
2019-11-29 00:31:28 +0530

    commit

--- RalphLauren/src/test/java/com/rl/qa/testcases/AccountLandingPageTest.java
+++ RalphLauren/src/test/java/com/rl/qa/testcases/AccountLandingPageTest.java
@@ -10,47 +10,42 @@ import com.rl.qa.pages.AccountLandingPage;
 import com.rl.qa.pages.FavoritesPage;
 import com.rl.qa.pages.LoginPage;
 
-public class AccountLandingPageTest extends TestBase
-{
+public class AccountLandingPageTest extends TestBase {
 	LoginPage loginpage;
 	AccountLandingPage accountlandingpage;
 	FavoritesPage favoritespage;
-	public AccountLandingPageTest()
-	{
+
+	public AccountLandingPageTest() {
 		super();
 	}
 
 	@BeforeMethod
-	public void setup()
-	{
+	public void setup() {
 		initialization();
 		loginpage = new LoginPage();
 		accountlandingpage = loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
 
-	@Test (priority=1)
-	public void verifyAccountLandingPageTitleTest()
-	{
+	@Test(priority = 1)
+	public void verifyAccountLandingPageTitleTest() {
 		String title = accountlandingpage.favPageTitle();
-		Assert.assertEquals(title, "Ralph Lauren - Account | Ralph Lauren | 103.1.11 - controllers\", \"account landing page did not match");
+		Assert.assertEquals(title,
+				"Ralph Lauren - Account | Ralph Lauren | 103.1.11 - controllers\", \"account landing page did not match");
 	}
 
-	@Test (priority=3)
-	public void navigateToFavPageTest()
-	{
+	@Test(priority = 3)
+	public void navigateToFavPageTest() {
 		favoritespage = accountlandingpage.navigateToFav();
 	}
 
-	@Test (priority = 2)
-	public void verifyCustomeNameTest()
-	{
+	@Test(priority = 2)
+	public void verifyCustomeNameTest() {
 		String custName = accountlandingpage.verifyUsername();
 		Assert.assertEquals(custName, "TEST", "username is not TEST");
 	}
 
 	@AfterMethod
-	public void close()
-	{
+	public void close() {
 		driver.quit();
 	}
 
